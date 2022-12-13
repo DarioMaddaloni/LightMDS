2dae29c5eaa2e372be7d8ff8597e1245dfba3e79
Giacomo-Fregona <giacomofregona99@gmail.com>
2022-12-13 10:30:18 +0100

    Sample

---
+++ Code/EM/EM_Main.py
@@ -0,0 +1,13 @@
+###   EXPECTATION MAXIMIZATION MAIN FILE
+
+
+import import_file
+
+
+#Opening the image
+imageName= "palla1"
+image = cv2.imread("../../Samples/EM/{}.png", 0)
+
+#Visualizing the image
+
+
---
+++ Code/show_lib.py
@@ -0,0 +1,5 @@
+import import_file
+
+def show(image):
+    plt.imshow(image,cmap='gray')
+    plt.show()
