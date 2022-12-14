27b65938de79f5d3bce4810fef60a0fca799edcd
DarioMaddaloni <dario.maddaloni@studenti.unitn.it>
2022-12-14 08:38:31 +0100

    Nuovo Commit

--- Code/EM/EM_main.py
+++ Code/EM/EM_main.py
@@ -8,6 +8,7 @@ from Code.import_file import *
 from Code.circle_lib import *
 from Code.show_lib import *
 import expectation_lib as ex
+import maximization_lib as ma
 
 
 #Opening the image
@@ -20,28 +21,44 @@ currentCy=186
 currentCircle = circle(currentCx, currentCy, currentRadius)
 threshold=1000
 
-points = ex.updateOfThePointsOfBoundary(image, currentCircle, threshold) #qui ci sono tutti i punti della circonferenza stimata
+points = [] #qui ci sono tutti i punti della circonferenza stimata
+allTheDk = []
+allTheWk = []
 
-AlltheDk = []
-AlltheWk = []
+#EXPECTATION
+points = ex.updateOfThePointsOfBoundary(image, currentCircle, threshold)
+for i in range(len(points)):
+	allTheDk.append(ex.deltak(points[i][0], points[i][1], currentCircle))
+currentEpsilon = ex.initializeEpsilon(image, currentCircle, threshold)
+currentSigma = ex.initializeSigma(allTheDk)
+for i in range(len(points)):
+	allTheWk.append(ex.wk(allTheDk[i], currentSigma, currentEpsilon))
+
+#MAXIMIZATION
+M = ma.computeM(points)
+W = ma.computeW(points, currentSigma, currentEpsilon, currentCircle)
+v = ma.computeEigenvector(M, W)
+currentCircle = ma.updateValues(v)
 
 
-for i in range(10): #qui ho messo una condizione a caso, ma sarà bene capire quando ferma expectation maximization
-	#Inizio la EXPECTATION
-	#Calcolo i deltak per ogni punto del bordo stimato
-	for i in range(len(points)):
-		AlltheDk.append(ex.deltak(points[0], points[1], circleObj))
-	#Calcolo l'epsilon attraverso il numero di punti totali
-	currentEpsilon = ex.updateEpsilon(image, currentCircle, threshold)
-	#Calcolo la sigma come descritto nel paper
-	currentSigma = ex.updateSigma(AlltheDk)
-	#Calcolo tutti i pesi di tutti i punti sul bordo stimato
-	for i in range(len(points)):
-		AlltheWk.append(ex.wk(AlltheDk[i], currentSigma, currentEpsilon))
-
-	#Inizio la MAXIMIZATION
-	#sigma = initializeSigma()
-	currentCircle.cx, currentCircle.cy, currentCircle.r = updateValues(v1,v2,v3,v4)
+#for i in range(10): #qui ho messo una condizione a caso, ma sarà bene capire quando ferma expectation maximization
+#	#Inizio la EXPECTATION
+#	#Trovo i punti del bordo della circonferenza
+#	points = ex.updateOfThePointsOfBoundary(image, currentCircle, threshold)
+#	#Calcolo i deltak per ogni punto del bordo stimato
+#	for i in range(len(points)):
+#		allTheDk.append(ex.deltak(points[0], points[1], circleObj))
+#	#Calcolo l'epsilon attraverso il numero di punti totali
+#	currentEpsilon = ex.updateEpsilon(image, currentCircle, threshold)
+#	#Calcolo la sigma come descritto nel paper
+#	currentSigma = ex.updateSigma(allTheDk)
+#	#Calcolo tutti i pesi di tutti i punti sul bordo stimato
+#	for i in range(len(points)):
+#		allTheWk.append(ex.wk(allTheDk[i], currentSigma, currentEpsilon))
+#
+#	#Inizio la MAXIMIZATION
+#	#sigma = initializeSigma()
+#	currentCircle.cx, currentCircle.cy, currentCircle.r = updateValues(v1,v2,v3,v4)
 
 #Visualizing the image
 show(ex.foundCircle(image, currentCircle, threshold))
--- Code/EM/expectation_lib.py
+++ Code/EM/expectation_lib.py
@@ -4,7 +4,7 @@ from Code.circle_lib import *
 
 def deltak(xk, yk, circleObj):
 	cx, cy, r = circleObj.cx, circleObj.cy, circleObj.r
-	return np.abs( (xk-cx)**2 + (yk - cy)**2 - r**2 );
+	return np.abs( (xk-cx)**2 + (yk - cy)**2 - r**2 )
 
 def wk(dk, sigma, epsilon):
 	value = np.exp((-(dk**2))/(2*(sigma**2)))
@@ -13,20 +13,20 @@ def wk(dk, sigma, epsilon):
 def initializeEpsilon(image, circleObj, threshold):
 	return 1 - (counterOfCirclePoints(image, circleObj, threshold))/(counterOfTotalPoints(image))
 
-#def initializeSigma(allTheDk):
-#	mean=np.sum(allTheDk)/len(allTheDk)
-#	total=0
-#	for i in range(len(allTheDk)):
-#		total=total+(allTheDk[i]*mean)**2
-#	return np.sqrt(total)/2
+def initializeSigma(allTheDk):
+	mean=np.sum(allTheDk)/len(allTheDk)
+	total=0
+	for i in range(len(allTheDk)):
+		total=total+(allTheDk[i]*mean)**2
+	return np.sqrt(total)/2
 
 def updateEpsilon(image, circleObj, threshold):
 	return initializeEpsilon(image, circleObj, threshold)
 
-def updateSigma(allTheWk, AlltheDk):
+def updateSigma(allTheWk, allTheDk):
 	allTheProduct=[];
 	for i in range(len(allTheWk)):
-		allTheProduct[i]=allTheWk[i]*AlltheDk[i]
+		allTheProduct[i]=allTheWk[i]*allTheDk[i]
 	return np.sum(allTheProduct)/np.sum(allTheWk)
 
 def counterOfTotalPoints(image):#da usare in initializeEpsilon ed updateEpsilon
